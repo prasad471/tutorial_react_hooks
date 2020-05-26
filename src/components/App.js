c2d47ed0ec5726f568f348918118553a46186c17
Bobbili <801269@cognizant.com>
2020-05-26 12:41:34 +0530

    refactoring class component to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ResourceList from './ResourceList';
+import ResourceList from "./ResourceList";
+import Users from "./Users";
 
-class App extends React.Component {
-  state = {
-    resourceName: 'posts'
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-        <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-        <ResourceList resourceName={this.state.resourceName}/>
-      </React.Fragment>
-    )
-  }
-}
+const App = () => {
+  const [resourceName, setResourceName] = useState("posts");
+  return (
+    <React.Fragment>
+      <Users />
+      <button onClick={() => setResourceName("posts")}>Posts</button>
+      <button onClick={() => setResourceName("todos")}>Todos</button>
+      <ResourceList resourceName={resourceName} />
+    </React.Fragment>
+  );
+};
 export default App;
